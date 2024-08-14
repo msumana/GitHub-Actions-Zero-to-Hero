047bd68e3da41f87f431ca83872b184857a91ddb
msumana <82926968+msumana@users.noreply.github.com>
2024-08-14 16:05:11 +0530

    Update addition.py
    Subtrct function added

--- src/addition.py
+++ src/addition.py
@@ -2,7 +2,14 @@
 # This is a test commit
 def add(a, b):
     return a + b
-
+    
+def subtract(a, b):
+    return a - b
+    
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+    
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(1, -1) == 2
